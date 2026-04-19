27d68cbf9ae52c21d64475e48de450329f9fcbf1
Rebecca Sandberg <rebeccasandberg02@gmail.com>
2026-04-19 09:14:12 +0200

    Files are creates
    Files are created and include the first step of the project layout to work with.

---
+++ index.js
@@ -0,0 +1,20 @@
+const bodyDOM = document.querySelector("body");
+const headerDOM = document.createElement("header");
+const mainDOM = document.createElement("main");
+const footerDOM = document.createElement("footer");
+
+bodyDOM.append(headerDOM);
+bodyDOM.append(mainDOM);
+bodyDOM.append(footerDOM);
+
+function header() { }
+
+function login() { }
+
+function register() { }
+
+function chat() { }
+
+function footer() { }
+
+login();
